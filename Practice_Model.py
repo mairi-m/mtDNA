feddd98e909ee62a40654f224abc70f0dd6d5f88
mairi-m <85343177+mairi-m@users.noreply.github.com>
2021-08-06 16:14:57 +0100

    playing around with mesa - didn't work
    The model was supposed to randomly print out the agents unique_id. It didn't work.

---
+++ Practice_Model.py
@@ -0,0 +1,52 @@
+# Title: Practice Model
+# Author: Mairi MacIain
+
+from mesa import Agent, Model
+from mesa.time import RandomActivation
+import matplotlib.pyplot as plt
+
+# Create agent class
+class TheAgent(Agent):
+    def __init__(self, unique_id, model):
+        self.unique_id = self.random.randint(1,100)
+        print(unique_id)
+
+
+    def step(self):
+        return
+
+
+class TheModel(Model):
+    def __init__(self, N):
+        self.num_agents = N
+        self.schedule = RandomActivation(self)
+
+        for index in range(self.num_agents):
+            a = TheAgent(index, self)
+            self.schedule.add(a)
+
+
+    def step(self):
+        self.schedule.step()
+
+TheModel(5)
+for index in range(5):
+    model.step()
+
+        
+        
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+        
+        
