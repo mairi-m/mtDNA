3219089c87b76e83b944bc7fedcedca358e06e5b
mairi-m <85343177+mairi-m@users.noreply.github.com>
2021-09-29 14:57:11 +0100

    used mesa agents, model and datacollector

--- Mesa_Population_graph_mairi.py
+++ Mesa_Population_graph_mairi.py
@@ -1,5 +1,6 @@
 import random
 from mesa import Agent, Model
+from mesa.time import RandomActivation
 from mesa.datacollection import DataCollector
 import matplotlib.pyplot as plt
 
@@ -20,26 +21,19 @@ class mtDNA(Agent):
       
         roll = random.random()
         if 0.0 < roll and roll < molecule.d:
-            print("Degrading "+str(molecule.unique_id)+" at timepoint "+str(i)+"!")
-          
+            print("Degrading "+str(molecule.unique_id))
             # Label molecule for removal
             self.model.molecules_to_remove.append(mol_index)
+            
+         elif molecule.d < roll and roll < molecule.r + molecule.d:
+            print("Replicating "+str(molecule.unique_id))
+            # Label mother for removal
+            self.model.molecules_to_remove.append(mol_index)
+            self.model.mother_mol_uniqueID(molecule.unique_id) # save mother id to give to daughter in model class
 
       
         
        
-        
-     
-
-        
-        
-        
-        
-        
-        
-# Initialise cell
-N0 = 20
-system_state=[mtDNA(unique_id=x,parent_id=-1) for x in range(0,N0)]
 
 
 class cell(Model):
@@ -54,51 +48,45 @@ class cell(Model):
     self.schedule.add(system_state)
     
     self.datacollector = DataCollector(
-      model_reporters = {"Number of agents" : 
+      model_reporters = {"Number of agents" : len(system_state)}
       agent_reporters = {"Status" : "status})
                                                 
     
-  def step(self):
+  def step(self):    
+    current_id = N0
     self.molecules_to_remove = []
-    self.molecules_to_replicate = []                     
-                         
-    self.datacollector.collect(self)
-    self.schedule.step()
-                
-    
+    self.mother_mol_uniqueID= []                     
+    self.new_molecules = []
 
-
-
-
-
-# Simulation
-current_id = N0 + 1
-# Assume time step = 1
-for i in range(0,10000): # like running the model 10000 times
-    molecules_to_remove = []
-    new_molecules = []
-    
-    for mol_ind in range(0,len(system_state)):
-        molecule = system_state[mol_ind]
-        
-        roll = random.random()
-        if 0.0 < roll and roll < molecule.d:
-            print("Degrading "+str(molecule.unique_id)+" at timepoint "+str(i)+"!")
-            # Label molecule for removal
-
-            molecules_to_remove.append(mol_ind)
-        elif molecule.d < roll and roll < molecule.r + molecule.d:
-            print("Replicating "+str(molecule.unique_id)+" at timepoint "+str(i)+"!")
-            # Label mother for removal
-            molecules_to_remove.append(mol_ind)
-            # Add two daughters
-            for j in range(2):
-                current_id += 1
-                daughter = mtDNA(current_id, r=molecule.r, d=molecule.d, parent_id=molecule.unique_id)
-                new_molecules.append(daughter)
+    for index in range(len(self.mother_mol_uniqueID):
+                       for j in range(2):
+                       current_id += 1
+                       daughter = mtDNA(current_id, r = 0.01, d = 0.01, parent_id = self.mother_mol_uniqueID[index]
+                       self.new_molecules.append(daughter)
+                       
+     
+                                        
                 
     # Drop all molecules which have been labelled for removal
     system_state = [mol for i,mol in enumerate(system_state) if i not in molecules_to_remove]
     # Add in the newly formed daughter molecules
     system_state = system_state + new_molecules
+                         
+                         
+                     
+    self.datacollector.collect(self)
+    self.schedule.step()
+                                        
+                                        
+   model = cell():
+  for index in range(100):
+           model.step()
+                                        
+  no_of_mtDNA = model.datacollector.get_model_vars_dataframe()
+no_of_mtDNA.plot()
+plt.show()
+                
+    
+
+
     
